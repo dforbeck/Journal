615a11d7687c1964299da51d6fa58d75f11aecce
Debbie <debbieforbeck@gmail.com>
2019-03-01 12:01:21 -0500

    Index page created

---
+++ Journal.Models/EntryListItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Journal.Models
+{
+    public class EntryListItem
+    {
+        public int EntryId { get; set; }
+        public string Title { get; set; }
+
+        [Display(Name = "Created")]
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+    }
+}
---
+++ Journal.WebMVC/Controllers/EntryController.cs
@@ -0,0 +1,20 @@
+﻿using Journal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Journal.WebMVC.Controllers
+{
+    [Authorize]
+    public class EntryController : Controller
+    {
+        // GET: Entry
+        public ActionResult Index()
+        {
+            var model = new EntryListItem[0];
+            return View(model);
+        }
+    }
+}
